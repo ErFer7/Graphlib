2541bf42443dd5c85527362e98a4ccc87ec91272
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-10-09 19:54:47 -0300

    Declarações de novos algoritmos

--- source/algorithms.py
+++ source/algorithms.py
@@ -15,8 +15,10 @@ def breadth_first_search(graph: Graph, s_index: int) -> tuple[dict, dict]:
     visited = [False] * graph.edge_count()
     distances = {}
     ancestors = {s_index: None}
+
     visited[s_index - 1] = True
     distances[s_index] = 0
+
     queue = []
     queue.append(s_index)
 
@@ -33,3 +35,66 @@ def breadth_first_search(graph: Graph, s_index: int) -> tuple[dict, dict]:
                 queue.append(v_index)
 
     return (distances, ancestors)
+
+
+def depth_first_search(graph: Graph, s_index: int) -> tuple[dict, dict]:
+    '''
+    Procura em profundidade.
+    [NÃO TESTADO]
+    '''
+
+    visited = [False] * graph.edge_count()
+    time = {}
+    ancestors = {s_index: None}
+
+    visited[s_index - 1] = True
+    time[s_index] = 0
+    total_time = 0
+
+    stack = []
+    stack.append(s_index)
+
+    while len(stack) != 0:
+
+        total_time += 1
+        u_vertex = stack.pop()
+        time[u_vertex] = total_time
+
+        for v_index in graph.neighbors(u_vertex):
+
+            if not visited[v_index - 1]:
+                visited[v_index - 1] = True
+                ancestors[v_index] = u_vertex
+                stack.append(v_index)
+
+    return (visited, time, ancestors)
+
+
+def hierholzer(graph: Graph) -> tuple[bool, list]:
+    '''
+    Algoritmo de Hierholzer para a busca de ciclos Eulerianos.
+    '''
+
+
+def bellman_held_karp(graph: Graph) -> list:
+    '''
+    Algoritmo de Bellman-Held-Karp.
+    '''
+
+
+def bellman_ford(graph: Graph, s_index: int) -> tuple[bool, dict, dict]:
+    '''
+    Algoritmo de Bellman-Ford.
+    '''
+
+
+def dijkstra(graph: Graph, s_index: int) -> tuple[dict, dict]:
+    '''
+    Algoritmo de Dijkstra.
+    '''
+
+
+def floyd_warshall(graph: Graph) -> tuple:
+    '''
+    Algoritmo de Floyd-Warshall.
+    '''
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -6,7 +6,7 @@ Testes de unidade.
 
 from os.path import join
 from source.graph import Graph
-from source.algorithms import breadth_first_search
+from source.algorithms import breadth_first_search, depth_first_search
 
 
 def test_bfs():
@@ -25,3 +25,21 @@ def test_bfs():
     assert distances_b == {1: 1, 2: 0, 3: 1, 4: 2} and ancestors_b == {1: 2, 2: None, 3: 2, 4: 3}
     assert distances_c == {1: 1, 2: 1, 3: 0, 4: 1} and ancestors_c == {1: 3, 2: 3, 3: None, 4: 3}
     assert distances_d == {1: 2, 2: 2, 3: 1, 4: 0} and ancestors_d == {1: 3, 2: 3, 3: 4, 4: None}
+
+
+# def test_dfs():
+#     '''
+#     ...
+#     '''
+
+#     graph = Graph(join("graphs", "test_graph.net"))
+
+#     visited_a, time_a, ancestors_a = depth_first_search(graph, 1)
+#     visited_b, time_b, ancestors_b = depth_first_search(graph, 2)
+#     visited_c, time_c, ancestors_c = depth_first_search(graph, 3)
+#     visited_d, time_d, ancestors_d = depth_first_search(graph, 4)
+
+#     assert all(visited_a) and time_a == {1: 1, 2: 3, 3: 2, 4: 4} and ancestors_a == {1: None, 2: 1, 3: 1, 4: 3}
+#     assert all(visited_b) and time_b == {1: 1, 2: 0, 3: 1, 4: 2} and ancestors_b == {1: 2, 2: None, 3: 2, 4: 3}
+#     assert all(visited_c) and time_c == {1: 1, 2: 1, 3: 0, 4: 1} and ancestors_c == {1: 3, 2: 3, 3: None, 4: 3}
+#     assert all(visited_d) and time_d == {1: 2, 2: 2, 3: 1, 4: 0} and ancestors_d == {1: 3, 2: 3, 3: 4, 4: None}
