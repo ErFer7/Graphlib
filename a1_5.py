d3d8a880e389068ceea49c0673af2d4155c75d53
yokiyoki87 <someyokisoba@gmail.com>
2022-10-09 23:00:14 -0300

    questao 5

--- a1_5.py
+++ a1_5.py
@@ -6,6 +6,11 @@ Exercício 5 [Algoritmo de Floyd-Warshall] da atividade 1.
 
 from os.path import join
 from source.graph import Graph
+from source.algorithms import min_path_Floyd_Warshall
 
 
-graph = Graph(join("graphs", "test_graph.net"))
+file = input("Grafo a ser usado: ")
+graph = Graph(join("graphs", file))
+D = min_path_Floyd_Warshall(graph)
+for i in range(1,len(D)+1):
+    print("%d:" % (i) +",".join(map(str,D[i-1])))
--- source/algorithms.py
+++ source/algorithms.py
@@ -33,3 +33,26 @@ def breadth_first_search(graph: Graph, s_index: int) -> tuple[dict, dict]:
                 queue.append(v_index)
 
     return (distances, ancestors)
+
+
+def min_path_Floyd_Warshall(graph:Graph):
+    D = []
+    D.append(matrix_W(graph))
+    size = graph.vertex_count()
+    for k in range(1,size+1):
+        D.append(matrix_empty(size))
+        for u in range(size):
+            for v in range(size):
+                D[k][u][v] = min(D[k-1][u][v],D[k-1][u][k-1]+D[k-1][k-1][v])
+    return D[-1]
+
+
+def matrix_W(graph:Graph):
+    D = graph.get_edges()
+    for i in range(graph.vertex_count()):
+        D[i][i] = 0
+    return D
+
+def matrix_empty(size:int):
+    return [[None for i in range(size)] for i in range(size)]
+    
