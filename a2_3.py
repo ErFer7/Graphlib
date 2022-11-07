d8e1852cbf2e497ce9fdc262e1ac0449a1eb4b27
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-11-06 23:24:07 -0300

    Conflito resolvido

--- a2_3.py
+++ a2_3.py
@@ -3,11 +3,8 @@
 '''
 Exercício 3 [Kruskal ou Prim] da atividade 2.
 '''
-<<<<<<< HEAD
-=======
 
 from math import inf
->>>>>>> bee9628b2f8e054ca9a2365549b2c1ed9b18698b
 from os.path import join
 from source.graph import Graph
 # from source.algorithms import kruskal
